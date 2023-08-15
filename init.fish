a5feb819b366f7b1156b1adcea4ea59b901cc452
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-08-15 20:59:03 +0800

    Add nvimpager as manpager.

--- init.fish
+++ init.fish
@@ -3,6 +3,7 @@
 fish_vi_key_bindings
 fish_config prompt choose informative
 fish_config theme choose Dracula
+set -gx PAGER nvimpager
 
 # Add functions
 for function in {$__fish_config_dir}/custom/functions/*.fish
