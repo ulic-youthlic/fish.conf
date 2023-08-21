588361ba85ec0cd4ea5d8aa820558e82432a1cdc
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-08-21 16:43:27 +0800

    Add powerline for fish terminal.

--- config.fish
+++ config.fish
@@ -8,3 +8,10 @@ set -gx PAGER nvimpager
 for function in {$__fish_config_dir}/custom/functions/*.fish
     source $function
 end
+
+# Add Powerline Fonts
+if test -e /usr/share/powerline/bindings/fish/powerline-setup.fish
+    set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+    source /usr/share/powerline/bindings/fish/powerline-setup.fish
+    powerline-setup
+end
