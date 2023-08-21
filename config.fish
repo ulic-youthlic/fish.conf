8e5ea3f9e094e8e6f6c4354d1da93057fecd8276
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-08-21 20:06:30 +0800

    Use more advanced method getting cwd in config.fish.

--- config.fish
+++ config.fish
@@ -1,9 +1,11 @@
 #!/usr/bin/env fish
+set -gx PAGER nvimpager
+set -l cwd (dirname (readlink -m (status --current-filename)))
+
 # Personal config
 fish_vi_key_bindings
 fish_config theme choose Dracula
-set -gx PAGER nvimpager
-set -l cwd (dirname (readlink -m (status --current-filename)))
+source {$cwd}/functions/fish_prompt.fish
 
 # Add functions
 set fish_function_path $fish_function_path "$cwd/functions"
