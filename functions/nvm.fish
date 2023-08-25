3ea5e8a139f720c707554757f76d411cef85042a
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-08-26 03:36:10 +0800

    Add default nvm path.

--- functions/nvm.fish
+++ functions/nvm.fish
@@ -1,3 +1,8 @@
+if test -e /usr/share/nvm
+    set -g NVM_DIR /usr/share/nvm
+else if test -e {$HOME}/.nvm
+    set -g NVM_DIR {$HOME}/.nvm
+end
 function nvm
     bass source {$NVM_DIR}/nvm.sh --no-use ';' nvm $argv
 end
