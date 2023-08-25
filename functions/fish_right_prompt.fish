d4e7f4e84a340c640d256b8dbc21fcb929eebc6b
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-08-26 03:32:37 +0800

    Add `TIME: ` before showing excuting time.

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt
     set -l duration_sec (math $CMD_DURATION / 1000)
-    printf '%s%gs' (set_color green --bold) $duration_sec
+    printf 'TIME: %s%gs' (set_color green --bold) $duration_sec
     set_color normal
 end
