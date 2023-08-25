329e77ef87911ed5791984629a22022f85f2a0ad
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-08-26 03:34:58 +0800

    Attach repo's function with more advanced method.

--- config.fish
+++ config.fish
@@ -6,7 +6,8 @@ set -U fish_greeting "Welcome! This is fish."
 # Personal config
 fish_vi_key_bindings
 fish_config theme choose Dracula
-source {$cwd}/functions/fish_prompt.fish
 
 # Add functions
-set fish_function_path $fish_function_path "$cwd/functions"
+set fish_function_path $fish_function_path[1]\
+                       $cwd/functions\
+                       $fish_function_path[2..-1]
