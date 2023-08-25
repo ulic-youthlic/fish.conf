edb931e0a9a55972198cc2f8e6899edb17e9723c
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-08-26 03:43:13 +0800

    Only without variable `NVM_DIR`, set it.

--- functions/nvm.fish
+++ functions/nvm.fish
@@ -1,7 +1,9 @@
-if test -e /usr/share/nvm
-    set -g NVM_DIR /usr/share/nvm
-else if test -e {$HOME}/.nvm
-    set -g NVM_DIR {$HOME}/.nvm
+if not set -q NVM_DIR
+    if test -e /usr/share/nvm
+        set -g NVM_DIR /usr/share/nvm
+    else if test -e {$HOME}/.nvm
+        set -g NVM_DIR {$HOME}/.nvm
+    end
 end
 function nvm
     bass source {$NVM_DIR}/nvm.sh --no-use ';' nvm $argv
