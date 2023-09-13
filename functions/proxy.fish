c59cffe5e52d99bbc031c9a50590ece5a3411403
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-09-13 11:16:10 +0800

    Modify proxy and unproxy functions

--- functions/proxy.fish
+++ functions/proxy.fish
@@ -1,6 +1,8 @@
-function proxy
-	set -gx http_proxy 'http://127.0.0.1:7890'
-	set -gx https_proxy 'http://127.0.0.1:7890'
-	set -gx HTTP_PROXY 'http://127.0.0.1:7890'
-	set -gx HTTPS_PROXY 'http://127.0.0.1:7890'
+if command -q clash
+    function proxy
+        set -gx http_proxy 'http://127.0.0.1:7890'
+        set -gx https_proxy 'http://127.0.0.1:7890'
+        set -gx HTTP_PROXY 'http://127.0.0.1:7890'
+        set -gx HTTPS_PROXY 'http://127.0.0.1:7890'
+    end
 end
--- functions/unproxy.fish
+++ functions/unproxy.fish
@@ -1,6 +1,8 @@
-function unproxy
-	set -e HTTP_PROXY
-	set -e HTTPS_PROXY
-	set -e https_proxy
-	set -e http_proxy
+if command -q clash
+    function unproxy
+        set -e HTTP_PROXY
+        set -e HTTPS_PROXY
+        set -e https_proxy
+        set -e http_proxy
+    end
 end
