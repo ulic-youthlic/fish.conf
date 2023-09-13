f29a2729bfe2247b4d8d35273ab78d36f3cde9ba
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-09-13 10:51:19 +0800

    Change theme from dracula to catppuccin

--- config.fish
+++ config.fish
@@ -5,7 +5,8 @@ set -U fish_greeting "Welcome! This is fish."
 
 # Personal config
 fish_vi_key_bindings
-fish_config theme choose Dracula
+fish_config theme choose "Catppuccin Macchiato"
+
 
 # Add functions
 set fish_function_path $fish_function_path[1]\
