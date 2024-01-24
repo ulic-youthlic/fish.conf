58ea811ca58b0265a1676f47ad4eb7994c7a11f9
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-24 23:12:09 +0800

    Add .cargo/bin, .ghcup/bin, .local/bin to PATH for rust, Haskell and python-pipx

--- config.fish
+++ config.fish
@@ -12,3 +12,18 @@ fish_config theme choose "Catppuccin Macchiato"
 set fish_function_path $fish_function_path[1]\
                        $cwd/functions\
                        $fish_function_path[2..-1]
+
+# Rust
+if command -q cargo
+    set PATH $PATH $HOME/.cargo/bin
+end
+
+# Haskell
+if command -q ghcup
+    set PATH $PATH $HOME/.ghcup/bin
+end
+
+# python-pipx
+if command -q pipx
+    set PATH $PATH $HOME/.local/bin
+end
