9b150783da5466fe68a997bee0afebd7b6419fe5
ulic-youthlic <ulic.youthlic@gmail.com>
2024-02-27 01:20:55 +0800

    change priority of proxy function

--- functions/proxy.fish
+++ functions/proxy.fish
@@ -1,12 +1,4 @@
-if command -q clash; or command -q mihomo
-    function proxy
-        echo 127.0.0.1
-        set -gx http_proxy 'http://127.0.0.1:7890'
-        set -gx https_proxy 'http://127.0.0.1:7890'
-        set -gx HTTP_PROXY 'http://127.0.0.1:7890'
-        set -gx HTTPS_PROXY 'http://127.0.0.1:7890'
-    end
-else if test -e $__fish_config_dir/.wsl
+if test -e $__fish_config_dir/.wsl
     function proxy
         set -l host_ip (ip route show | grep -i default | awk '{ print $3}')
         echo $host_ip
@@ -14,5 +6,13 @@ else if test -e $__fish_config_dir/.wsl
         set -xg HTTP_PROXY $http_proxy
         set -xg https_proxy $http_proxy
         set -xg HTTPS_PROXY $http_proxy
+    end
+else if command -q clash; or command -q mihomo
+    function proxy
+        echo 127.0.0.1
+        set -gx http_proxy 'http://127.0.0.1:7890'
+        set -gx https_proxy 'http://127.0.0.1:7890'
+        set -gx HTTP_PROXY 'http://127.0.0.1:7890'
+        set -gx HTTPS_PROXY 'http://127.0.0.1:7890'
     end
 end
