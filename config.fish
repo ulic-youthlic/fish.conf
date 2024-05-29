b542169dc956c67d8dab69086ef41695e0a70dc3
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-05-29 08:11:34 +0800

    format config.fish

--- config.fish
+++ config.fish
@@ -13,10 +13,10 @@ fish_config theme choose "Catppuccin Macchiato"
 
 
 # Add functions
-set fish_function_path $fish_function_path[1]\
-                       $cwd/functions\
-                       $fish_function_path[2..-1]
+set fish_function_path $fish_function_path[1] \
+    $cwd/functions \
+    $fish_function_path[2..-1]
 
 for conf in $cwd/conf.d/*.fish
-	source $conf
+    source $conf
 end
