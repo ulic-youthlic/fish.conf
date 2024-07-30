97ec8dc5792dec7c9a660f217f95c48881a40d2a
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-07-30 22:32:47 +0800

    add $CARGO_HOME as test condition of rust config

--- conf.d/0.00_rust.fish
+++ conf.d/0.00_rust.fish
@@ -1,5 +1,9 @@
 if command -q cargo
-    fish_add_path -amg $HOME/.cargo/bin
+    if set -q CARGO_HOME
+        fish_add_path -amg $CARGO_HOME/bin
+    else
+        fish_add_path -amg $HOME/.cargo/bin
+    end
 else
     _warn_no_command cargo
 end
